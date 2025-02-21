c6edbd30f60dc676db6b0eee9f1436bd1571629a
김보찬 <t01029181258@gmail.com>
2025-02-22 00:13:36 +0900

    fuckingdone222

--- functions/checkSubmission.js
+++ functions/checkSubmission.js
@@ -15,9 +15,19 @@ export const handler = async (req,context) => {
 
     // 상수 리스트로 제출 데이터를 정의 (data.csv 대신 사용)
     const submissions = [
-        { name: '홍길동', number: '1234', description: '서류 제출 완료' },
-        { name: '이몽룡', number: '5678', description: '심사 중' },
-        { name: '성춘향', number: '9876', description: '최종 합격' }
+        { name: '최민서', number: '1293', description: '(기타)영상 2개 제출 완료' },
+        { name: '박시은', number: '6065', description: '(보컬)영상 2개 제출 완료' },
+        { name: '김동건', number: '8272', description: '(기타)미제출' },
+        { name: '강다영', number: '9034', description: '(보컬)영상 2개 제출 완료' },
+        { name: '김가은', number: '8516', description: '(키보드)미제출' },
+        { name: '박서영', number: '1621', description: '(드럼)영상 2개 제출 완료' },
+        { name: '임수연', number: '3807', description: '(보컬)미제출' },
+        { name: '조민서', number: '2572', description: '(키보드)영상 1개 제출 완료' },
+        { name: '오주하', number: '1880', description: '(보컬)영상 2개 제출 완료' },
+        { name: '유다연', number: '5818', description: '(보컬)영상 1개 제출 완료' },
+        { name: '오도영', number: '6855', description: '(기타)영상 1개 제출 완료' },
+        { name: '김윤진', number: '5614', description: '(보컬)미제출' },
+        { name: '김동규', number: '4714', description: '(드럼)영상 1개 제출 완료' }
     ];
 
     // 입력값과 일치하는 항목 검색
